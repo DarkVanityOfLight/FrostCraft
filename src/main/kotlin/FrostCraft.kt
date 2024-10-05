4a1d2df06b10c09145cad7f6df95e7ad3aa1c1eb
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-10-05 15:21:07 +0200

    feat: Register and deregister players on leave/join

--- src/main/kotlin/FrostCraft.kt
+++ src/main/kotlin/FrostCraft.kt
@@ -5,13 +5,15 @@ import net.axay.kspigot.main.KSpigot
 import net.kyori.adventure.bossbar.BossBar
 
 class FrostCraft : KSpigot() {
-
     companion object {lateinit var  INSTANCE: FrostCraft; private set}
+    lateinit var playerManager: PlayerManager
 
     override fun load() {
     }
 
     override fun startup() {
+
+        playerManager = PlayerManager()
     }
 
     override fun shutdown() {
--- src/main/kotlin/PlayerManager.kt
+++ src/main/kotlin/PlayerManager.kt
@@ -1,8 +1,21 @@
+import net.axay.kspigot.event.listen
 import org.bukkit.entity.Player
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 import java.util.UUID
 
 class PlayerManager {
     private val players = mutableMapOf<UUID, FrostPlayer>()
+
+    init {
+        listen<PlayerJoinEvent> {
+            registerPlayer(it.player)
+        }
+
+        listen<PlayerQuitEvent> {
+            deregisterPlayer(it.player)
+        }
+    }
 
     fun registerPlayer(player: Player){
         players[player.uniqueId] = FrostPlayer(player)
