eef5d462818ea902939704e61057b3759060cad1
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-10-05 17:52:51 +0200

    feat: Basics to take damage from the cold

--- src/main/kotlin/FrostPlayer.kt
+++ src/main/kotlin/FrostPlayer.kt
@@ -1,5 +1,34 @@
-import org.bukkit.entity.Player
+import net.axay.kspigot.event.listen
+import org.bukkit.Bukkit
+import org.bukkit.damage.DamageSource
+import org.bukkit.damage.DamageType
+import org.bukkit.event.entity.EntityDamageEvent
+
+const val dmg = 0.5;
 
 class FrostPlayer(var playerId: java.util.UUID) {
-    var temperature : Float = 0.0f
+    var temperature : Float = -1.0f
+    var diedFromFrost = false
+
+    fun updateTemperature(){}
+
+    fun checkTemperature(){
+
+        val player = Bukkit.getPlayer(playerId)
+        if (temperature < 0){
+            val source = DamageSource.builder(DamageType.FREEZE).build()
+            val event = EntityDamageEvent(player!!, EntityDamageEvent.DamageCause.FREEZE, source, dmg)
+            Bukkit.getPluginManager().callEvent(event)
+
+            if (!event.isCancelled){
+                if (player.health - dmg <= 0){
+                    player.health = 0.0
+                    diedFromFrost = true
+                }else{
+                    player.health = player.health - dmg
+                }
+            }
+
+        }
+    }
 }
--- src/main/kotlin/PlayerManager.kt
+++ src/main/kotlin/PlayerManager.kt
@@ -1,5 +1,8 @@
 import net.axay.kspigot.event.listen
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import java.util.UUID
@@ -15,6 +18,24 @@ class PlayerManager {
         listen<PlayerQuitEvent> {
             deregisterPlayer(it.player)
         }
+
+        listen<PlayerDeathEvent>{
+            if (it.entity.uniqueId in players){
+               if (players[it.entity.uniqueId]!!.diedFromFrost){
+                   it.deathMessage = "${it.entity.name} died from the cold"
+                   players[it.entity.uniqueId]!!.diedFromFrost = false
+               }
+            }
+        }
+
+        // Add a task to check the temperature of every player every second
+        // This is a simple way to do it, but it's not efficient, it's better to use a more efficient way to do it
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(Manager, Runnable {
+            players.values.forEach {
+                it.checkTemperature()
+
+            }
+        }, 0, 20)
     }
 
     fun registerPlayer(player: Player){
