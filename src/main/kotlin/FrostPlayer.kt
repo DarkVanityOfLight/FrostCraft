b8b7b343ddd706ef826480dd230fcc3631c1713c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-10-05 18:57:01 +0200

    feat: Send message when freezing

--- src/main/kotlin/FrostPlayer.kt
+++ src/main/kotlin/FrostPlayer.kt
@@ -1,3 +1,5 @@
+import net.axay.kspigot.chat.KColors
+import net.axay.kspigot.chat.literalText
 import net.axay.kspigot.event.listen
 import org.bukkit.Bukkit
 import org.bukkit.damage.DamageSource
@@ -9,15 +11,31 @@ const val dmg = 0.5;
 class FrostPlayer(var playerId: java.util.UUID) {
     var temperature : Float = -1.0f
     var diedFromFrost = false
+    var coldMessageInterval = 0
 
     fun updateTemperature(){}
 
     fun checkTemperature(){
+        val player = Bukkit.getPlayer(playerId)!!
 
-        val player = Bukkit.getPlayer(playerId)
+
         if (temperature < 0){
+            if (coldMessageInterval == 0 ){
+                player.sendActionBar(literalText("You are cold!"){
+                    italic = true
+                    color = KColors.LIGHTBLUE
+                })
+            }
+
+            coldMessageInterval++;
+
+
+            if(coldMessageInterval >= 20){
+                coldMessageInterval = 0
+            }
+
             val source = DamageSource.builder(DamageType.FREEZE).build()
-            val event = EntityDamageEvent(player!!, EntityDamageEvent.DamageCause.FREEZE, source, dmg)
+            val event = EntityDamageEvent(player, EntityDamageEvent.DamageCause.FREEZE, source, dmg)
             Bukkit.getPluginManager().callEvent(event)
 
             if (!event.isCancelled){
