bb5770a0e452e32f99df11628617533f5b85f27f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-10-06 17:01:50 +0200

    feat: Add some basic temperature updates depending on insulation

--- src/main/kotlin/FrostPlayer.kt
+++ src/main/kotlin/FrostPlayer.kt
@@ -1,38 +1,42 @@
 import net.axay.kspigot.chat.KColors
 import net.axay.kspigot.chat.literalText
-import net.axay.kspigot.event.listen
 import net.axay.kspigot.extensions.geometry.toSimple
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.damage.DamageSource
 import org.bukkit.damage.DamageType
 import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageEvent
 
 const val dmg = 0.5;
+const val bodyTemp = 37.0f
 
 class FrostPlayer(var playerId: java.util.UUID) {
     var temperature : Float = 0.0f
     var diedFromFrost = false
-    var coldMessageInterval = 0
+    private var coldMessageInterval = 0
 
-    fun updateTemperature(){}
+    private fun updateTemperature(heatSources: Set<Material>, insulation : List<Material>, zoneTemperature: Float){
+        temperature = zoneTemperature + (calculateInsulation(insulation) * (calculateHeatSources(heatSources) + bodyTemp))
+    }
 
 
     // Do this async(I hate sync/async stuff)
     fun checkTemperature(player: Player){
         assert(player.uniqueId == playerId)
 
-        if (player.gameMode == org.bukkit.GameMode.CREATIVE || player.gameMode == org.bukkit.GameMode.SPECTATOR){
-            return
+
+        val (isEnclosed, insulation) = isEnclosed(player.location.toSimple(), player.world)
+
+        // If we are enclosed we care about insulation if not we don't
+        if (isEnclosed){
+            updateTemperature(emptySet(), insulation, Manager.climateManager.getTemperature(player.location))
+        }else {
+            updateTemperature(emptySet(), emptyList(), Manager.climateManager.getTemperature(player.location))
         }
 
-        isEnclosed(player.location.toSimple(), player.world).let {
-            temperature = if (it.first){
-                this.coldMessageInterval = 0
-                0.0f
-            }else{
-                -1.0f
-            }
+        if (player.gameMode == org.bukkit.GameMode.CREATIVE || player.gameMode == org.bukkit.GameMode.SPECTATOR){
+            return
         }
 
         // Apply effects in sync on the next tick
@@ -44,7 +48,7 @@ class FrostPlayer(var playerId: java.util.UUID) {
 
     // Run this in sync
     private fun applyTemperatureEffects(player: Player) {
-        if (temperature < 0){
+        if (temperature < bodyTemp){
             if (coldMessageInterval == 0 ){
                 player.sendActionBar(literalText("You are cold!"){
                     italic = true
