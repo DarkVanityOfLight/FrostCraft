1c4e15026e3bd06ee40655aa6cc6b0854591a252
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-10-06 17:50:03 +0200

    feat: Add the climate manager

--- src/main/kotlin/FrostCraft.kt
+++ src/main/kotlin/FrostCraft.kt
@@ -1,11 +1,15 @@
 import net.axay.kspigot.chat.literalText
+import net.axay.kspigot.commands.argument
 import net.axay.kspigot.commands.command
 import net.axay.kspigot.commands.runs
+import net.axay.kspigot.extensions.geometry.SimpleLocation3D
+import net.axay.kspigot.extensions.geometry.toSimple
 import net.axay.kspigot.main.KSpigot
 
 class FrostCraft : KSpigot() {
     companion object {lateinit var  INSTANCE: FrostCraft; private set}
     lateinit var playerManager: PlayerManager
+    lateinit var climateManager: ClimateManager
 
     override fun load() {
         INSTANCE = this
